cfac502c84530eaa67013169219877d7f53d7bcf
Divyansh Tripathi <devxd12@gmail.com>
2026-07-20 10:13:57 +0530

    Time: 3 ms (10.69%), Space: 19.1 MB (98.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -6,13 +6,12 @@ class Solution:
 
             for j in range(len(nums)-1-i):
 
-                if nums[j+1]>nums[j]:
+                if nums[j]>nums[j+1]:
                     nums[j],nums[j+1]=nums[j+1],nums[j]
                     swapped=  True
 
             if not swapped:
                 break
-        nums.reverse()
         return nums
         """
         Do not return anything, modify nums in-place instead.
